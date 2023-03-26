506efb98cc275eaedf25dbffc10d64dae1db7895
hperrin <hperrin@student.42.fr>
2023-03-26 16:02:05 +0200

    Ex03 finished and tested

--- src/vector/ex03.rs
+++ src/vector/ex03.rs
@@ -7,6 +7,24 @@ where
     K: Clone + Sum,
     for<'a> &'a K: Mul<&'a K, Output = K>,
 {
+    ///
+    /// Returns the dot product of the the two vectors.
+    ///
+    /// If you're sure that your input is valid, you can use [dot](Vector::dot).
+    ///
+    /// # Example
+    /// ```
+    /// use matrix::Vector;
+    /// use matrix::error::VectorOperationError;
+    ///
+    /// let vec1 = Vector::from([4, 2, 3]);
+    /// let vec2 = Vector::from([2, 3]);
+    /// assert_eq!(vec1.safe_dot(&vec2), Err(VectorOperationError::NotSameSize(3, 2)));
+    /// ```
+    ///
+    /// Complexity:
+    /// Linear: O(n) with `n` the total number of coordinates of the vectors.
+    ///
     pub fn safe_dot(&self, v: &Self) -> Result<K, VectorOperationError> {
         if self.len() != v.len() {
             Err(VectorOperationError::NotSameSize(self.len(), v.len()))
@@ -19,7 +37,7 @@ where
     /// Returns the dot product of the the two vectors.
     ///
     /// Using the [safe_dot](Vector::safe_dot) method, you get a [Result], whereas this function
-    /// will return a wrong answer in case of a bad input
+    /// will return a wrong answer in case of a bad input.
     ///
     /// # Example
     /// ```
@@ -44,5 +62,46 @@ where
             .zip(v.content.iter())
             .map(|(lhs, rhs)| lhs * rhs)
             .sum()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::Vector;
+
+    #[test]
+    fn example() {
+        {
+            let u = Vector::from([0., 0.]);
+            let v = Vector::from([1., 1.]);
+            let res = u.safe_dot(&v).unwrap();
+            assert_eq!(res, 0.0);
+            println!("{}", res);
+        }
+        {
+            let u = Vector::from([1., 1.]);
+            let v = Vector::from([1., 1.]);
+            let res = u.safe_dot(&v).unwrap();
+            assert_eq!(res, 2.0);
+            println!("{}", res);
+        }
+        {
+            let u = Vector::from([-1., 6.]);
+            let v = Vector::from([3., 2.]);
+            let res = u.safe_dot(&v).unwrap();
+            assert_eq!(res, 9.0);
+            println!("{}", res);
+        }
+    }
+
+    #[test]
+    fn error() {
+        let u = Vector::from([0., 0., 1.]);
+        let v = Vector::from([1., 1.]);
+        let res = u.safe_dot(&v);
+        assert_eq!(
+            res,
+            Err(crate::error::VectorOperationError::NotSameSize(3, 2))
+        );
     }
 }
