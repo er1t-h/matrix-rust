289da9e30cae11c942cd2dfb9bafd12740ef6f01
hperrin <hperrin@student.42.fr>
2023-03-31 16:26:43 +0200

    Did the row echelon, but needed reduced row echelon

--- src/matrix/ex10.rs
+++ src/matrix/ex10.rs
@@ -7,41 +7,58 @@ use super::Dimensions;
 impl<K> Matrix<K>
 where
     for<'a> K: Clone + Default + MulAssign<&'a K> + SubAssign<&'a K>,
-    for<'a> &'a K: PartialEq + Mul<&'a K, Output = K> + Div<&'a K, Output = K>,
-    for<'a> &'a mut K: IsZero,
+    for<'a> &'a K: PartialEq + Mul<&'a K, Output = K> + Div<&'a K, Output = K> + IsZero,
 {
     pub fn row_echelon(&self) -> Self {
         // First, we skip all zero columns
-        let mut first_non_zero_column = self.dimensions.width;
-        let mut first_non_zero_line = 0;
-        for i in 0..self.dimensions.width {
-            if self.get_column(i).unwrap().any(|x| x != &K::default()) {
-                first_non_zero_column = i;
-                first_non_zero_line = self
+        let mut first_non_zero_column = 0;
+        let mut rows_set = 0;
+        let default_elt = K::default();
+        let mut return_matrix = self.clone();
+        while first_non_zero_column < self.dimensions.width {
+            let mut first_non_zero_line = 0;
+            let mut end = true;
+            for i in first_non_zero_column..self.dimensions.width {
+                if self
                     .get_column(i)
                     .unwrap()
-                    .position(|x| x != &K::default())
-                    .unwrap();
-                break;
+                    .skip(rows_set)
+                    .any(|x| x != &default_elt)
+                {
+                    end = false;
+                    first_non_zero_column = i;
+                    first_non_zero_line = self
+                        .get_column(i)
+                        .unwrap()
+                        .skip(rows_set)
+                        .position(|x| x != &default_elt)
+                        .unwrap()
+                        + rows_set;
+                    break;
+                }
             }
-        }
-        let mut return_matrix = self.clone();
-        if first_non_zero_column == self.dimensions.width {
-            return return_matrix;
-        }
-        return_matrix.swap_line(0, first_non_zero_line);
-        for non_treated_line in 1..return_matrix.dimensions.height {
-            let coeff = {
-                let first_number_of_new_line = return_matrix
-                    .get(non_treated_line, first_non_zero_column)
-                    .unwrap();
-                let pivot = return_matrix.get(0, first_non_zero_column).unwrap();
-                first_number_of_new_line / pivot
-            };
-            for elt_index in first_non_zero_column..return_matrix.dimensions.width {
-                let tmp = &coeff * return_matrix.get(0, elt_index).unwrap();
-                *return_matrix.get_mut(non_treated_line, elt_index).unwrap() -= &tmp;
+            if end {
+                return return_matrix;
             }
+            return_matrix.swap_line(rows_set, first_non_zero_line);
+            rows_set += 1;
+            for non_treated_line in rows_set..return_matrix.dimensions.height {
+                let coeff = {
+                    let first_number_of_new_line = return_matrix
+                        .get(non_treated_line, first_non_zero_column)
+                        .unwrap();
+                    if first_number_of_new_line.is_zero() {
+                        continue;
+                    }
+                    let pivot = return_matrix.get(0, first_non_zero_column).unwrap();
+                    first_number_of_new_line / pivot
+                };
+                for elt_index in first_non_zero_column..return_matrix.dimensions.width {
+                    let tmp = &coeff * return_matrix.get(0, elt_index).unwrap();
+                    *return_matrix.get_mut(non_treated_line, elt_index).unwrap() -= &tmp;
+                }
+            }
+            first_non_zero_column += 1;
         }
         return_matrix
     }
