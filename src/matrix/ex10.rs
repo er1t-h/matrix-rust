5a103358d364601625bd7330c13d8e1286a20e02
hperrin <hperrin@student.42.fr>
2023-04-03 12:22:51 +0200

    Row echelon working

--- src/matrix/ex10.rs
+++ src/matrix/ex10.rs
@@ -1,4 +1,4 @@
-use std::ops::{Div, Mul, MulAssign, SubAssign};
+use std::ops::{Div, DivAssign, Mul, MulAssign, SubAssign};
 
 use crate::{traits::IsZero, Matrix};
 
@@ -6,24 +6,23 @@ use super::Dimensions;
 
 impl<K> Matrix<K>
 where
-    for<'a> K: Clone + Default + MulAssign<&'a K> + SubAssign<&'a K>,
+    for<'a> K: Clone + Default + MulAssign<&'a K> + SubAssign<&'a K> + DivAssign<&'a K>,
     for<'a> &'a K: PartialEq + Mul<&'a K, Output = K> + Div<&'a K, Output = K> + IsZero,
 {
     pub fn row_echelon(&self) -> Self {
         // First, we skip all zero columns
         let mut first_non_zero_column = 0;
         let mut rows_set = 0;
-        let default_elt = K::default();
         let mut return_matrix = self.clone();
         while first_non_zero_column < self.dimensions.width {
             let mut first_non_zero_line = 0;
             let mut end = true;
             for i in first_non_zero_column..self.dimensions.width {
-                if self
+                if return_matrix
                     .get_column(i)
                     .unwrap()
                     .skip(rows_set)
-                    .any(|x| x != &default_elt)
+                    .any(|x| !x.is_zero())
                 {
                     end = false;
                     first_non_zero_column = i;
@@ -31,7 +30,7 @@ where
                         .get_column(i)
                         .unwrap()
                         .skip(rows_set)
-                        .position(|x| x != &default_elt)
+                        .position(|x| !x.is_zero())
                         .unwrap()
                         + rows_set;
                     break;
@@ -41,6 +40,17 @@ where
                 return return_matrix;
             }
             return_matrix.swap_line(rows_set, first_non_zero_line);
+            let first_copy = return_matrix
+                .get(rows_set, first_non_zero_column)
+                .unwrap()
+                .clone();
+            for elt in return_matrix
+                .get_line_mut(rows_set)
+                .unwrap()
+                .skip(first_non_zero_column)
+            {
+                *elt /= &first_copy;
+            }
             rows_set += 1;
             for non_treated_line in rows_set..return_matrix.dimensions.height {
                 let coeff = {
@@ -62,6 +72,11 @@ where
         }
         return_matrix
     }
+
+    // pub fn reduced_row_echelon(&self) -> Self {
+    //     let return_matrix = self.row_echelon();
+
+    // }
 }
 
 #[cfg(test)]
@@ -79,7 +94,7 @@ mod test {
         {
             let u = Matrix::from([[1., 2.], [3., 4.]]);
             let res = u.row_echelon();
-            assert_eq!(res, [[1.0, 2.0], [0.0, -2.0]]);
+            assert_eq!(res, [[1.0, 2.0], [0.0, 1.0]]);
             println!("Row echelon of {u} = {res}");
         }
         {
